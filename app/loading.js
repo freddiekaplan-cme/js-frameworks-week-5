595e518ebe979486631e46022c35be868b0f71cf
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-03-03 10:59:18 +0100

    first commit

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+export default function loading() {
+	return (
+		<>
+			<div className="loading">
+				<h1>Loading...</h1>
+			</div>
+		</>
+	)
+}
---
+++ app/portfolio/page.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+    	<div className="page-content portfolio">
+			<h1>Portfolio</h1>
+			<p>lorem ipsum dolor sit amet, consectetur adipis.</p>
+			<div>
+				<Image
+					src="/js-frameworks-week-1.png"
+					alt="Assignment 1 screenshot thumbnail"
+					width={478}
+					height={231}
+					priority
+				/>
+			</div>
+			<div>
+				<Image
+					src="/js-frameworks-week-2.png"
+					alt="Assignment 1 screenshot thumbnail"
+					width={478}
+					height={231}
+					//priority
+				/>
+			</div>
+    	</div>
+	)
+}
