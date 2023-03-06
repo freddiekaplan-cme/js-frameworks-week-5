46d8723855bad75e59c4abba389eeb4f81155f18
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-03-06 10:06:36 +0100

    Fix med next fonts.

--- app/contact/page.js
+++ app/contact/page.js
@@ -40,50 +40,7 @@ export default function Home() {
 					<button type="submit">Submit</button>
 				</Form>
 			</Formik>
-			
-			{/* <Formik
-       			initialValues={{ email: "", password: "" }}
-       			validate={values => {
-					const errors = {};
-
-					if (!values.email) {
-						errors.email = "Required";
-					} else if (
-						!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-					) {
-						errors.email = "Invalid email address";
-					}
-					return errors;
-				}}
-
-				onSubmit={(values, { setSubmitting }) => {
-					setTimeout(() => {
-					alert(JSON.stringify(values, null, 2));
-					setSubmitting(false);
-					}, 400);
-				}}
-			>
-
-				{({ isSubmitting }) => (
-					<Form>
-
-						<Field type="name" name="name" />
-						<ErrorMessage name="name" component="div" />
-
-						<Field type="email" name="email" />
-						<ErrorMessage name="email" component="div" />
-
-						<Field name="message" as="textarea" className="form-textarea" />
-
-						<button type="submit" disabled={isSubmitting}>
-						Submit
-						</button>
-
-					</Form>
-				)}
-
-     		</Formik> */}
-
+	
 		</div>
 	)
 }
