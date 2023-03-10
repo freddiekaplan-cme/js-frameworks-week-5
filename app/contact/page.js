1b607a173ed23f7c9576860e3008d3931a27eccd
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-03-10 10:58:47 +0100

    Changed form to regular html, form validation using Yup.

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,65 +1,95 @@
-import React from 'react'
-import { Formik, Form, Field } from 'formik'
-import * as Yup from 'yup'
+"use client"
+import React from "react"
+import { Formik, Form, Field } from "formik"
+import * as Yup from "yup"
+import form from "../form.module.css"
 
-const schema = Yup.object().shape({
+const ContactSchema = Yup.object().shape({
   fullName: Yup.string()
-  .required('Full Name is required'),
-  email: Yup.string()
-  .email('Invalid email')
-  .required('Email is required'),
-  message: Yup.string()
-  .required('Message is required')
+	.min(2, "Please write more than that.")
+	.max(50, "That's a bit long.")
+	.required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  message: Yup.string().min(10, "Please write more than that.").required("Required"),
 })
 
-export default function Home({ data }) {
+export default function Home() {
+  const handleSubmit = async (values) => {
+	const { fullName, email, message } = values
+	const emailBody = `Full Name: ${fullName}\n\nEmail: ${email}\n\nMessage: ${message}`
+	window.location.href = `mailto:freddie.kaplan@edu.cmeducations.se?subject=Contact Form&body=${emailBody}`
+  }
+
   return (
-	<div>
+	<div className="page-content about">
 	  <h1>Contact</h1>
-	  <Formik
-		initialValues={{
-		  fullName: '',
-		  email: '',
-		  message: ''
-		}}
-		validationSchema={schema}
-		onSubmit={async (values) => {
-		  const res = await fetch('/api/form', {
-			method: 'POST',
-			headers: {
-			  'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(values)
-		  })
-		  const data = await res.json()
-		  if (!res.ok) alert(data.msg)
-		  else alert('Success')
-		}}
-	  >
-		{(formik) => (
-		  <Form>
-			<label htmlFor='fullName'>Name</label>
-			<Field name='fullName' />
-			{formik.touched.fullName && formik.errors.fullName ? (
-			  <div>{formik.errors.fullName}</div>
-			) : null}
 
-			<label htmlFor='email'>Email</label>
-			<Field type='email' name='email' />
-			{formik.touched.email && formik.errors.email ? (
-			  <div>{formik.errors.email}</div>
-			) : null}
+	  <div className="card">
+		<div className={form.text}>
+		  Get in touch with me about internships, work or anything else.
+		</div>
+		<Formik
+		  initialValues={{
+			fullName: "",
+			email: "",
+			message: "",
+		  }}
+		  onSubmit={handleSubmit}
+		  validationSchema={ContactSchema}
+		>
+		  {({ errors, touched }) => (
+			<Form className={form.contactForm}>
+			  <div>
+				<label htmlFor="fullName">Name</label>
+				<br />
+				<Field
+				  id="fullName"
+				  name="fullName"
+				  className={`${form.input} ${
+					errors.fullName && touched.fullName ? form.error : ""
+				  }`}
+				/>
+				{errors.fullName && touched.fullName && (
+				  <div className={form.errorMessage}>{errors.fullName}</div>
+				)}
+			  </div>
+			  <div>
+				<label htmlFor="email">Email</label>
+				<br />
+				<Field
+				  id="email"
+				  name="email"
+				  type="email"
+				  className={`${form.input} ${
+					errors.email && touched.email ? form.error : ""
+				  }`}
+				/>
+				{errors.email && touched.email && (
+				  <div className={form.errorMessage}>{errors.email}</div>
+				)}
+			  </div>
+			  <div>
+				<label htmlFor="message">Message</label>
+				<br />
+				<Field
+				  name="message"
+				  as="textarea"
+				  className={`${form.input} ${form.textarea} ${
+					errors.message && touched.message ? form.error : ""
+				  }`}
+				/>
+				{errors.message && touched.message && (
+				  <div className={form.errorMessage}>{errors.message}</div>
+				)}
+			  </div>
 
-			<label htmlFor='message'>Message</label>
-			<Field name='message' as='textarea' />
-			{formik.touched.message && formik.errors.message ? (
-			  <div>{formik.errors.message}</div>
-			) : null}
-
-			<button type='submit'>Submit</button>
-		  </Form>
-		)}
-	  </Formik>
+			  <button type="submit" className={form.button}>
+				Submit
+			  </button>
+			</Form>
+		  )}
+		</Formik>
+	  </div>
 	</div>
   )
 }
